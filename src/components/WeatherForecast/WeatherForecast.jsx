ac38d862d515856c37691bb6a1f78288b79a1592
kiran1926 <kiranroge19@gmail.com>
2025-03-25 21:48:12 -0500

    wip: added logic for WeatherForecast component

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,16 @@
+import "./WeatherForecast.css";
+
+const WeatherForecast = (props) => {
+    const { day, img, imgAlt, conditions, time } = props;
+    return (
+        <div className="card">
+            <h2 className="day-header">{day}</h2>
+            <img className="img-class" src={img} alt={imgAlt} aria-label={`weather icon for ${conditions}`}></img>
+            <p className="conditions"> Conditions: {conditions} </p>
+            <p>Time: {time}</p>
+        </div>
+
+    );
+};
+
+export default WeatherForecast;
